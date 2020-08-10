046b3b88537b0ce38ba4fef1c1dc628c3f82649e
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-08-09 18:45:35 -0700

    iterative sorting/seraching complete - MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,18 +7,27 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+              
 
         # TO-DO: swap
         # Your code here
-
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    swapped = True
+    while swapped:
+        swapped = False
+        for index in range(1,len(arr)):
+            if arr[index] < arr[index - 1]:
+                arr[index], arr[index - 1] = arr[index - 1], arr[index]
+                swapped = True
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr) ):
+        if arr[i] == target:
+          return i
 
     return -1   # not found
 
@@ -9,6 +11,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    if len(arr) == 0:
+        return -1 # array empty
+    
+    low = 0
+    high = len(arr)-1
+    while high > low:
+      midpoint = (high + low) // 2
+      if arr[midpoint] == target:
+          return midpoint
+      elif arr[midpoint] < target:
+          low = midpoint
+      elif arr[midpoint] > target:
+        high = midpoint
+  # TO-DO: add missing code
 
     return -1  # not found
